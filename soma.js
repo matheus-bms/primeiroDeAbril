4899349439d9a8214b4ecea54936589d309e9fc5
matheus <math_bms@hotmail.com>
2021-04-05 11:42:15 -0300

    adiciona filter na soma

--- soma.js
+++ soma.js
@@ -1,1 +1,26 @@
 console.log('Hello world')
+
+
+const countries = [
+    {
+        country: 'madagascar',
+        population: 10000
+    },{
+        country: 'uganda',
+        population: 45000
+    },{
+        country: 'ruanda',
+        population: 11000
+    },{
+        country: 'burundi',
+        population: 12000
+    }
+]
+
+
+function countryPopulation(population){
+    return countries.filter(x=>{
+        return x.population > population
+    })
+}
+console.log(countryPopulation(20000))
